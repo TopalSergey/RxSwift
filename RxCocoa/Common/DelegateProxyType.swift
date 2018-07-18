d071dd283a4d9e13ce7747786e86b37518b429b9
Sergey Topal <topal.sergey.sm@gmail.com>
2018-07-18 12:41:24 +0300

    Fix: navBar animation breaked when use tableview datasource

--- RxCocoa/Common/DelegateProxyType.swift
+++ RxCocoa/Common/DelegateProxyType.swift
@@ -325,8 +325,6 @@ extension DelegateProxyType where ParentObject: HasPrefetchDataSource, Self.Dele
                 , DelegateProxy.Delegate: AnyObject {
                 let proxy = DelegateProxy.proxy(for: object)
                 let unregisterDelegate = DelegateProxy.installForwardDelegate(dataSource, retainDelegate: retainDataSource, onProxyForObject: object)
-                // this is needed to flush any delayed old state (https://github.com/RxSwiftCommunity/RxDataSources/pull/75)
-                object.layoutIfNeeded()
 
                 let subscription = self.asObservable()
                     .observeOn(MainScheduler())
